e90e465f889d6cf882b0d890443046d12d42582d
caenjiro <cejimenez41@ucatolica.edu.co>
2025-05-16 08:22:02 -0500

    Add files via upload
    Archivo de configuración Base de datos

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'clave_secreta'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://UserPlataforma:Hola12@DESKTOP-57PO3BN/plataformacursos'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
