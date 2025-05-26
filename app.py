00b8003caadb4904e69ab1da0e921caeb76d887b
caenjiro <cejimenez41@ucatolica.edu.co>
2025-05-26 18:27:50 -0500

    Prueba de despliegue continuo

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ def generar_podcast(id):
         ruta_podcast, transcripcion = resultado
 
           
-        # ✅ GUARDAR EN BASE DE DATOS MY SQL DESPLEIGUE AUTOMATICO
+        # ✅ GUARDAR EN BASE DE DATOS MY SQL DESPLEIGUE AUTOMATICO EN LA PRESENTACION
         ruta_podcast = resultado[0].replace("\\", "/")
         archivo.ruta_podcast = ruta_podcast
         archivo.transcripcion = transcripcion
